defafa43899c1f736e0bc68524090a2ff74d51eb
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-21 19:09:20 +0700

    initialize index.js

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import MainLayout from './../layouts/MainLayout';
+import BlankLayout from '../layouts/BlankLayout';
+import HomePage from './../pages/HomePage';
+import AccountPage from './../pages/AccountPage';
+import UserProfilePage from './../pages/UserProfilePage';
+import LoginPage from './../pages/LoginPage';
+import RegisterPage from './../pages/RegisterPage';
+import NotFoundPage from './../pages/NotFoundPage';
+
+function Router() {
+  return (
+    <Routes>
+        {/* first định nghĩa các Route có main layout */}
+        <Route path="/" element={<MainLayout/>}>
+            <Route index element={<HomePage/>}></Route>
+            <Route path="account" element={<AccountPage/>}></Route>
+            <Route path="user/:userId" element={<UserProfilePage/>}></Route>
+        </Route>
+
+        <Route path="/" element={<BlankLayout/>}>
+            <Route path="/login" element={<LoginPage/>}></Route>
+            <Route path="/register" element={<RegisterPage/>}></Route>
+            <Route path="*" element={<NotFoundPage/>}></Route>
+        </Route>
+    </Routes>
+
+  )
+}
+
+export default Router
