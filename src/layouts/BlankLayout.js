1bb22fe3c29b1831b609ccce34b5c424203dea96
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-23 12:25:05 +0700

    revise 'BlankLayout' appearance

--- src/layouts/BlankLayout.js
+++ src/layouts/BlankLayout.js
@@ -1,13 +1,14 @@
+import { Stack } from '@mui/system'
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Logo from '../components/Logo'
 
 function BlankLayout() {
   return (
-    <div>
-        <h1>
-            BlankLayout
-        </h1>
-
-    </div>
+    <Stack minHeight="100vh" jsutifyContent="center" alignItems="center">
+        <Logo sx={{width:90, height: 90, mb: 5}}/>
+        <Outlet></Outlet>
+    </Stack>
   )
 }
 
