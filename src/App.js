5ed212eaabab52189ae0c5b0e2ae53bdb4a8388e
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-24 10:39:22 +0700

    import ThemeProvider in @App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import Router from './routes'
+import ThemeProvider from "./theme";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Router/>
+        <ThemeProvider>
+          <Router/>
+        </ThemeProvider>
       </BrowserRouter>
     </AuthProvider>
   )
