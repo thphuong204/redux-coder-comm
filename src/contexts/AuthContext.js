33e692c01cd1310d463579d08a5d17235f785b71
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-27 08:12:04 +0700

    update AccountPage, AuthContext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,5 @@
 import {createContext, useReducer, useEffect} from 'react';
+import { useSelector } from 'react-redux';
 import apiService from '../app/apiService';
 import {isValidToken} from '../utils/jwt';
 
@@ -43,7 +44,41 @@ const reducer = (state,action) => {
                 isAuthenticated:false,
                 user: null,
             };
-            
+        case UPDATE_PROFILE:
+          const {
+            name,
+            avatarUrl,
+            coverUrl,
+            aboutMe,
+            city,
+            country,
+            company,
+            jobTitle,
+            facebookLink,
+            instagramLink,
+            linkedinLink,
+            twitterLink,
+            friendCount,
+            postCount
+          } = action.payload;
+          return {...state,
+            user:{...state.user,
+              name,
+              avatarUrl,
+              coverUrl,
+              aboutMe,
+              city,
+              country,
+              company,
+              jobTitle,
+              facebookLink,
+              instagramLink,
+              linkedinLink,
+              twitterLink,
+              friendCount,
+              postCount
+            }}
+
         default:
             return state;
     }
@@ -64,7 +99,7 @@ const setSession = (accessToken) => {
 function AuthProvider ({children}) {
 
     const [state,dispatch]  = useReducer(reducer,initialState);
-
+    const updatedProfile = useSelector((state)=> state.user.updatedProfile)
 
     useEffect(() => {
         const initialize = async () => {
@@ -106,6 +141,11 @@ function AuthProvider ({children}) {
     
         initialize();
       }, []);
+    
+    useEffect(() => {
+      if (updatedProfile)
+      dispatch({type:UPDATE_PROFILE,payload:updatedProfile});
+    },[updatedProfile]);
 
     const login = async({email,password}, callback) => {
         const response = await apiService.post("/auth/login", {email,password});
